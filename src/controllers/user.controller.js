e18ee4ae0a3bbe4bc2f3dcfc3f3bc3ce7fec0592
HarshadPrajapati <harshad00912@gmail.com>
2024-08-11 19:03:18 +0530

    add ApiResponse

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -67,7 +67,8 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   return res.status(201).json(
-    new ApiResponse(200, createdUser, "User registered Successfully")
+    new ApiResponse(200, createdUser, 
+      "User registered Successfully")
   )
 
 
